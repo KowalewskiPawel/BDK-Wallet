6cf552fb06de52d4a78cb9297d7a8df9ca42790d
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-07-03 21:49:21 +0200

    add transaction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use bdk::{bitcoin::Network, database::MemoryDatabase, wallet::AddressIndex, Wallet};
+use std::str::FromStr;
+
+use bdk::{
+    bitcoin::{Address, Network},
+    blockchain::{ElectrumBlockchain, Blockchain},
+    database::MemoryDatabase,
+    electrum_client::Client,
+    wallet::{AddressIndex, Wallet},
+    SyncOptions, SignOptions,
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let external_descriptor = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/0'/0'/0/*)";
@@ -13,6 +22,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let address = wallet.get_address(AddressIndex::New)?;
     println!("Generated Address: {}", address);
+
+    let client = Client::new("ssl://electrum.blockstream.info:60002")?;
+    let blockchain = ElectrumBlockchain::from(client);
+
+    wallet.sync(&blockchain, SyncOptions::default())?;
+
+    let balance = wallet.get_balance()?;
+    println!("Wallet balance in SAT: {}", balance);
+
+    let faucet_address = Address::from_str("mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt")?;
+
+    let mut tx_builder = wallet.build_tx();
+    tx_builder
+        .add_recipient(faucet_address.script_pubkey(), 1000)
+        .enable_rbf();
+    let (mut psbt, tx_details) = tx_builder.finish()?;
+
+    println!("Transaction details: {:#?}", tx_details);
+
+    let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
+    assert!(finalized, "Tx has not been finalized");
+    println!("Transaction Signed: {}", finalized);
+
+    let raw_transaction = psbt.extract_tx();
+    let txid = raw_transaction.txid();
+    blockchain.broadcast(&raw_transaction)?;
+    println!(
+        "Transaction sent! TXID: {txid}.\nExplorer URL: https://blockstream.info/testnet/tx/{txid}",
+        txid = txid
+    );
 
     Ok(())
 }
