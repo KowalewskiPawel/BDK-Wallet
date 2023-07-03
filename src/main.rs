8b4c6f1d5a73b3a765844ce3c4cbdb26607341c0
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-07-03 21:04:10 +0200

    add wallet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bdk::{bitcoin::Network, database::MemoryDatabase, wallet::AddressIndex, Wallet};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let external_descriptor = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/0'/0'/0/*)";
+    let internal_descriptor = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/0'/0'/1/*)";
+
+    let wallet: Wallet<MemoryDatabase> = Wallet::new(
+        external_descriptor,
+        Some(internal_descriptor),
+        Network::Testnet,
+        MemoryDatabase::new(),
+    )?;
+
+    let address = wallet.get_address(AddressIndex::New)?;
+    println!("Generated Address: {}", address);
+
+    Ok(())
 }
